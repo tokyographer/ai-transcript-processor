e38eb75da688fa5f4517f87e39ff003c67225ae3
Anthony T <tokyographer@gmail.com>
2024-06-14 09:39:14 +0200

    Basic CRM Install

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React from 'react';
-import 'flowbite';
-import './index.css';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1">
+        <Header />
+        <MainContent />
+      </div>
     </div>
   );
 }
