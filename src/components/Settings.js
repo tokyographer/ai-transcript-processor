1783a6f5a5a47615571ce25eee1ad452bc05739a
Anthony T <tokyographer@gmail.com>
2024-06-15 16:18:42 +0200

    Created Settings.js

---
+++ src/components/Settings.js
@@ -0,0 +1,15 @@
+// src/components/Settings.js
+
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-semibold mb-4">Settings</h2>
+      <p>Settings content goes here.</p>
+      {/* Add your settings form or functionality here */}
+    </div>
+  );
+}
+
+export default Settings;
