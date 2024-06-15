53dd72e753fb907372386af53f537e3fa957e689
Anthony T <tokyographer@gmail.com>
2024-06-15 16:32:09 +0200

    Corrected package.json

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
+// src/App.js
+
 import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Contacts from './components/Contacts';
 import Settings from './components/Settings'; // Import the Settings component
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-    <div className="flex h-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Header />
-        <Routes>
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <Routes>
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/settings" element={<Settings />} /> {/* Add the Settings route */}
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  </Router>
+    </Router>
   );
 }
 
